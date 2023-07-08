164ddd9a6adb6c70637f260d3cbd40fac479fcfc
MohamedNifrad <67473203+MohamedNifrad@users.noreply.github.com>
2023-07-08 12:54:25 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,60 +1,22 @@
-import {FormsModule,ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-
-
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-
-//datatables module
-import { DataTablesModule } from 'angular-datatables';
-
-
-//import layouts module
-import { LayoutsModule } from './layouts/layouts.module';
-import { EmployeesComponent } from './employees/employees.component';
-
-
-//fire base
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { environment } from '../environments/environment';
-
-import { EmployeesService } from './services/employees.service'
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {AppService} from "./app.service";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    LoginComponent,
-    RegisterComponent,
-    DashboardComponent,
-    EmployeesComponent
+    AppComponent
   ],
-  
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
     HttpClientModule,
-    ReactiveFormsModule,
-    //import LayoutsModule
-    LayoutsModule,
-    //
-    DataTablesModule,
-
-    //fire store
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule
-
+    FormsModule
   ],
-  providers: [EmployeesService],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
